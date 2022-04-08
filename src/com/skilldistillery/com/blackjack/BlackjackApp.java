f613a1990b601e156d37a9b1a897bf09c711e752
Erich Schindler <erschindler@gmail.com>
2022-04-08 15:27:14 -0400

    first commit

---
+++ src/com/skilldistillery/com/blackjack/BlackjackApp.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.com.blackjack;
+
+public class BlackjackApp {
+
+	public static void main(String[] args) {
+		BlackjackApp bj = new BlackjackApp();
+		bj.run();
+	}
+
+	public void run() {
+		// run the program
+	}
+
+}
